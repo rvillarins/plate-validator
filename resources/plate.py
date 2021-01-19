fa3a9262f8edff87010ef97a76518a12a0782afd
rvillarins <rvillarins@gmail.com>
2021-01-19 10:50:48 -0300

    Problem: method to validate plates was not running in the code. solution: call the "validadePlate" method and setup the HTTP response code for each of the given cases.

--- resources/plate.py
+++ resources/plate.py
@@ -2,22 +2,31 @@ import json, datetime
 from flask_restful import Resource, reqparse
 from models.Plate import plateModel
 
-#class that returns all initial json data from the list to the routing: /plates
+# GET /plates
 class getPlates(Resource):
     def get(self):
         return {'plates': [plate.json() for plate in plateModel.query.all()]}
 
+# GET /plates/<string:plate_id>
 class postPlates(Resource):
     def post(self, plate_id):                                                                    
             if plateModel.checkExisting(plate_id):
                 return{'message':'Plate {} already exists.'.format(plate_id)}, 400
             newPlate = plateModel(plate_id)
+            testPlate = newPlate.validatePlate(plate_id)
             try:
-                newPlate.save_plate(), 201
+                if testPlate == 200:
+                    newPlate.save_plate(), 200
+                    return {'plate':'{}'.format(plate_id)}
+                elif testPlate == 422:
+                    return {'message':'{} is not a valid German Plate.'.format(plate_id)}, 422
+                else:
+                    return 400
+
             except:
                 return {'message':'an internal error ocurred trying to save the plate.'}, 500
             
-            return {'plate':'{}'.format(plate_id)} # return the new variable and de 201 created code 201 # return the new variable and de 201 created code
+            
             
 
    
